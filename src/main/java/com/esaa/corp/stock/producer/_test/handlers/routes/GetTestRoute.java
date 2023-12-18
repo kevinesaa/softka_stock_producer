fed018713fccf4d5fc139cb77b558aa935ea8875
Kevin Esaa <kevin.esaa@gmail.com>
2023-12-18 11:18:38 -0400

    feature: test endpoint

---
+++ src/main/java/com/esaa/corp/stock/producer/_test/handlers/routes/GetTestRoute.java
@@ -0,0 +1,27 @@
+package com.esaa.corp.stock.producer._test.handlers.routes;
+
+import com.esaa.corp.stock.producer._test.model.TestDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class GetTestRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> testEndpoint(){
+        return RouterFunctions.route(
+                RequestPredicates.GET("/test"),
+                request ->
+                   ServerResponse.ok()
+                           .contentType(MediaType.APPLICATION_JSON)
+                           .bodyValue(new TestDTO("hello"))
+        );
+    }
+}
---
+++ src/main/java/com/esaa/corp/stock/producer/_test/model/TestDTO.java
@@ -0,0 +1,21 @@
+package com.esaa.corp.stock.producer._test.model;
+
+public class TestDTO {
+
+    private String message;
+
+    public TestDTO() {
+    }
+
+    public TestDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
