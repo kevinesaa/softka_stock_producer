52a0201df374e05d6cfdac3cb3c33486d50b1843
Kevin Esaa <kevin.esaa@gmail.com>
2023-12-19 14:13:27 -0400

    feature: db env properties

--- src/main/java/com/esaa/corp/stock/producer/ProducerApplication.java
+++ src/main/java/com/esaa/corp/stock/producer/ProducerApplication.java
@@ -1,10 +1,15 @@
 package com.esaa.corp.stock.producer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ProducerApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
---
+++ src/main/java/com/esaa/corp/stock/producer/_commons/models/database/Batch.java
@@ -0,0 +1,50 @@
+package com.esaa.corp.stock.producer._commons.models.database;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+public class Batch implements Serializable {
+
+    //@Id
+    private String id;
+    private String reason;
+    private List<ItemMovement> items;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public List<ItemMovement> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemMovement> items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Batch batch = (Batch) o;
+        return Objects.equals(id, batch.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
